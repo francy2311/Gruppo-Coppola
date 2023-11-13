f37f6fd1c247a08d623532ef50e1e2246bf1a1f1
Francy2311 <francesco.alo23@gmail.com>
2023-11-13 16:57:03 +0100

    Changes to be committed:

--- CRUD.java
+++ CRUD.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 /**
  * CRUD
  */
 public class CRUD {
-    //Scrittura dei dati in un file
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Login code here
+
+        // Ask user for information
+        System.out.println("Inserisci le informazioni:");
+        String info = input.nextLine();
+
+        // Do something with the information
+        System.out.println("Hai inserito: " + info);
+
+        input.close();
+    }
 }
