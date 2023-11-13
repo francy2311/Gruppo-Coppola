ef1c46069e0f3b52c005b4ffdfe3f8d122e2b7b7
Francy2311 <francesco.alo23@gmail.com>
2023-11-13 17:03:50 +0100

    Commit Francesco

---
+++ CRUD.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+/**
+ * CRUD
+ */
+public class CRUD {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        // Login code here
+
+        // Ask user for information
+        System.out.println("Inserisci le informazioni:");
+        String info = input.nextLine();
+
+        // Do something with the information
+        System.out.println("Hai inserito: " + info);
+
+        input.close();
+        
+        System.out.println("Ciao!");
+        System.out.println("Ciao!");
+    }
+}
