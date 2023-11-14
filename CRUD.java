c0ab0e521d9f42cae7bed175f86af69b22c82b2d
Francesco Aloisio <francesco.aloisio@mail.polimi.it>
2023-11-14 12:16:05 +0100

    Commit Classe CRUD

--- CRUD.java
+++ CRUD.java
@@ -3,21 +3,38 @@ import java.util.Scanner;
 /**
  * CRUD
  */
-public class CRUD {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+public class CRUD extends Utente {
+    
+    private String nome;
+    private String cognome;
+    private String email;
 
-        // Login code here
-
-        // Ask user for information
-        System.out.println("Inserisci le informazioni:");
-        String info = input.nextLine();
-
-        // Do something with the information
-        System.out.println("Hai inserito: " + info);
-
-        input.close();
-        System.out.println("Ciao");
-        System.out.println("Mi chiamo francesco");
+    public CRUD(String username,String password,String nome,String cognome,String email){
+        super(username,password);
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
     }
+
+    @Override
+    public String toString(){
+        return "Username"+getUsername()+"Nome: "+nome+" Cognome: "+cognome+" Email: "+email;
+    }
+
+    public void modificaInformazioni(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Inserisci il nome: ");
+        this.nome = input.nextLine();
+        System.out.println("Inserisci il cognome: ");
+        this.cognome = input.nextLine();
+        System.out.println("Inserisci l'email: ");
+        this.email = input.nextLine();
+    }
+    
+    public void cancellaInformazioni(){
+        this.nome = "";
+        this.cognome = "";
+        this.email = "";
+    }
+    
 }
