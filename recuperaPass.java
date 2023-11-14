8136f3f27b76f4cef57b06a091b6b594eafec0ca
Sergio Castiglione <149104403+scasti521@users.noreply.github.com>
2023-11-14 12:22:35 +0100

    Commit con modifiche

--- recuperaPass.java
+++ recuperaPass.java
@@ -1,22 +1,23 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class recuperaPass {
-    public void recupero(Utente U){
+    public void recupero(ArrayList<Utente> ListaUtenti){
         Scanner sc= new Scanner(System.in);
         System.out.println("Inserisci il tuo username:");
         String username= sc.nextLine();
-        if (username.equals(U.getUsername())) {
-            System.out.println("Inserisci la tua vecchia password: ");
-            String vecchiaPass= sc.nextLine();
-
-            if (vecchiaPass.equals(U.getPassword())) {
+        for(int i=0; i<ListaUtenti.size(); i++){
+            if (username.equals(ListaUtenti.get(i).getUsername())) {   
+                System.out.println("Inserisci la tua vecchia password: ");
+                String vecchiaPass= sc.nextLine();
+            if (vecchiaPass.equals(ListaUtenti.get(i).getPassword())) { 
                 System.out.println("Insersci la nuova password: ");
                 String nuovaPass= sc.nextLine();
                 System.out.println("Conferma la nuova password: ");
                 String confermaPass= sc.nextLine();
 
                 if(nuovaPass.equals(confermaPass)){
-                    U.setPassword(nuovaPass);
+                    ListaUtenti.get(i).setPassword(nuovaPass);
                 }else{
                     System.out.println("Le password non coincidono");
                 } 
@@ -24,5 +25,7 @@ public class recuperaPass {
                 System.out.println("Username errato/non esistente");
             }
         }
+        }
+       
     }
 }
